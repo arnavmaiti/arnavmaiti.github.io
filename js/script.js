3c428916c9acc3cb8704f5d7e068f95180529e64
Arnav Maiti <arnavmaiti@gmail.com>
2025-06-28 20:04:33 -0700

    feat: more components

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(() => {
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-  if (prefersDark) {
+  const themeLocalStorage = localStorage.themeMode;
+  if (!themeLocalStorage) {
+    localStorage.themeMode = prefersDark ? "dark" : "light";
+  }
+  const darkMode = themeLocalStorage ? themeLocalStorage === "dark" : prefersDark;
+  if (darkMode) {
     $('html').css('color-scheme', 'dark');
     $('.toggle-button a.toggle-mode i.fa-solid').removeClass('fa-sun').addClass('fa-moon');
   } else {
@@ -14,9 +19,14 @@ $(document).ready(() => {
     if (currentColorScheme === 'dark') {
       $('html').css('color-scheme', 'light');
       $('.toggle-button a.toggle-mode i.fa-solid').removeClass('fa-moon').addClass('fa-sun');
+      localStorage.themeMode = "light";
     } else {
       $('html').css('color-scheme', 'dark');
       $('.toggle-button a.toggle-mode i.fa-solid').removeClass('fa-sun').addClass('fa-moon');
+      localStorage.themeMode = "dark";
     }
+  });
+  $('.feature.feature-portfolio').click(() => {
+    window.location = "/";
   });
 });
